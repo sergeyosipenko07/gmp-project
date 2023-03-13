0936b1d7f524cab5e41dc492f148b6f53e361f12
Siarhei Asipenka <siarhei_asipenka@epam.com>
2023-03-13 13:22:12 +0200

    Init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { CoursesComponent } from './components/courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
